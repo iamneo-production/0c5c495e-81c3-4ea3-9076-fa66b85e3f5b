2a23df0e32db7c422537460f7177bac9c0afb22b
Batcave765 <msriram765@gmail.com>
2023-07-11 09:59:59 +0000

    changed src

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Login from "./Components/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import Register from "./Components/Register";
+import React, { useLayoutEffect } from "react";
+import Profile from "./Components/Profile";
+import Footer from "./Components/Footer";
+import Waterintake from "./Components/Waterintake";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	useLayoutEffect(() => {
+		document.body.style.backgroundColor = "white";
+	});
+	return (
+		<BrowserRouter>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/profile" element={<Profile />} />
+				<Route path="/waterintake" element={<Waterintake />} />
+				<Route path="*" element={<h1>404 Not Found</h1>} />
+			</Routes>
+			<Footer />
+		</BrowserRouter>
+	);
 }
 
 export default App;
